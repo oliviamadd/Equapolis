40d74e422cf6dd3d19a567fdf627ccb4a896a56a
oliviamadd <81091207+oliviamadd@users.noreply.github.com>
2021-03-25 13:04:34 -0400

    Update

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -1,4 +1,10 @@
-/*!
+ <script>
+   $('#myTab a').click(function (e) {
+     e.preventDefault()
+     $(this).tab('show')
+   })
+</script>
+ /*!
   * Bootstrap v4.5.3 (https://getbootstrap.com/)
   * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
